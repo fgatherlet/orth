0e7e56aa9e7bfa1b53638cfa874a6b32660b5234
dice-k-nakatani <dice.k.nakatani@gmail.com>
2019-04-12 13:05:45 +0900

    Initial Commit

---
+++ orth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  orth
+//
+//  Created by dn0174 on 2019/04/12.
+//  Copyright © 2019 fgatherlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
